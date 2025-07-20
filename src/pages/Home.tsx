e34f0601fe305ffa0147e411b8c90677b592c196
Brad Gyngell <brad.gyngell@hevi.app>
2025-07-20 14:29:07 +1000

    Photo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -70,7 +70,7 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Video Section */}
+      {/* Video Section
       <section className="py-16 px-4 bg-card relative">
         <div className="absolute top-0 left-0 w-full overflow-hidden">
           <svg
@@ -108,12 +108,24 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Bio Section */}
-      <section className="py-16 px-4 bg-background">
+      <section className="pb-16 pt-32 px-4 bg-card relative">
+        <div className="absolute top-0 left-0 w-full overflow-hidden">
+          <svg
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+            className="relative block w-full h-[60px]"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              className="fill-colour-2"
+            ></path>
+          </svg>
+        </div>
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
             <div>
               <h2 className="font-serif text-4xl font-semibold text-foreground mb-6">
                 Meet Robbie
@@ -147,7 +159,7 @@ const Home = () => {
             </div>
             <div className="relative">
               <img
-                src="/lovable-uploads/60395de9-eb19-46f9-81c3-375ca13b178b.png"
+                src="/Robbie_Photo.png"
                 alt="Robbie Gyngell performing with guitar and microphone"
                 className="aspect-square object-cover rounded-lg shadow-lg"
               />
